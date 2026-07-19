fc9af11a292559e9e1cec4b353d49495c7b75d5f
SarcasticLabrador <bauer.mark@mail.de>
2026-07-19 09:52:27 +0200

    Update home.py

--- frontend/views/home.py
+++ frontend/views/home.py
@@ -15,7 +15,7 @@ def _scoring_rules() -> None:
     st.markdown("**Individual matches**")
     st.markdown(
         "**Group stage**\n"
-        "- Correct tendency (win / draw / loss): **2 points**
+        "- Correct tendency (win / draw / loss): **2 points**"
         " (e.g. 8 points for predicting France, Spain, England, and Argentina to make it to the semi-finals \n"
         "- Correct exact scoreline: **+3 points** (total 5 if exact)\n\n"
         "**Knockout rounds**\n"
